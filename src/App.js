447e9df495112498c1ce53b2d1077b0ac98827a8
SchmidtPeti <schmidt.peti32@gmail.com>
2023-03-27 18:51:44 +0200

    set up the main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,104 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import axios from 'axios';
+
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const [data, setData] = useState([]);
+
+  const handleSearch = async () => {
+    // Here, you'd typically make an API request to fetch data based on the searchTerm.
+    // For the purpose of this example, we'll just use dummy data.
+    const dummyData = [
+      {
+        id: 1,
+        title: 'Card 1',
+        image: 'https://via.placeholder.com/150',
+        score: 4.5
+      },
+      {
+        id: 2,
+        title: 'Card 2',
+        image: 'https://via.placeholder.com/150',
+        score: 3.7
+      }
+    ];
+    const apiUrl = 'https://api.citatum.hu/idezet.php';
+    const params = {
+      konyv: "tüskevár",
+      f: 'Mondvanolvaso',
+      j: 'c53d6e70479b94bf1d1a4bc872eb2bd7'
+    };
+  
+    try {
+      const response = await axios.get(apiUrl, { params: params });
+  
+      // Parse XML response using DOMParser
+      const parser = new DOMParser();
+      const xmlDoc = parser.parseFromString(response.data, 'application/xml');
+  
+      const idezet = xmlDoc.querySelector('idezet');
+      const idezetszoveg = idezet.querySelector('idezetszoveg').textContent;
+      const szerzo = idezet.querySelector('szerzo').textContent;
+      const kategoriak = Array.from(idezet.querySelectorAll('kategoria')).map(
+        (elem) => elem.textContent
+      );
+      const kedvenc = idezet.querySelector('kedvenc').textContent;
+      const id = idezet.querySelector('id').textContent;
+      const url = idezet.querySelector('url').textContent;
+  
+      console.log('idezetszoveg:', idezetszoveg);
+      console.log('szerzo:', szerzo);
+      console.log('kategoriak:', kategoriak);
+      console.log('kedvenc:', kedvenc);
+      console.log('id:', id);
+      console.log('url:', url);
+    } catch (error) {
+      console.error('Error fetching quote:', error);
+    }
+
+    setData(dummyData);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container d-flex justify-content-center align-items-center min-vh-100">
+      <div className="row">
+        <div className="col-12 text-center">
+          <h1>Search Example</h1>
+        </div>
+        <div className="col-12 d-flex justify-content-center">
+          <input
+            type="text"
+            className="form-control"
+            id="search-input"
+            placeholder="Search here..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button className="btn btn-primary ms-2" onClick={handleSearch}>
+            Search
+          </button>
+        </div>
+        <div className="col-12 mt-4">
+          {data.map((item) => (
+            <div key={item.id} className="card mb-3" style={{ maxWidth: '540px' }}>
+              <div className="row g-0">
+                <div className="col-md-4">
+                  <img src={item.image} alt={item.title} />
+                </div>
+                <div className="col-md-8">
+                  <div className="card-body">
+                    <h5 className="card-title">{item.title}</h5>
+                    <p className="card-text">Score: {item.score}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
